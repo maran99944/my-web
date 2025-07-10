c013198b2b0d7753d21fc31f43e2ce6ae4e84ddf
Manimaran <134256862+maran99944@users.noreply.github.com>
2025-07-11 00:12:01 +0530

    Update kubectl.sh

--- scripts/kubectl.sh
+++ scripts/kubectl.sh
@@ -13,5 +13,5 @@ curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable
 # Install kubectl and set correct permissions
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-# Verify the installation
+# Verify the installation--
 kubectl version --client
